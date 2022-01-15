80e8775be079bc95aad8ac733971d8f7a689e77d
Steven <steven928@hotmail.fr>
2022-01-15 20:19:51 +0100

    add a config, merge imdb files and sort it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import urllib.request
 import gzip
 import os
+import pandas as pd
+import configparser
+
 
 # Downloads a file from an URL and saves it to a local file.
 def download_file(url, file_name):
@@ -35,12 +38,46 @@ def download_and_unzip_imdb():
     unzip_gz("data/imdb/title.basics.tsv.gz")
         
 
-def main():
+# Matches the tconst from the title tsv with the tconst from the ratings tsv.
+def match_tconst():
+    # Open the files
+    basics = pd.read_csv('data/imdb/title.basics.tsv', sep='\t', header=0, dtype={'tconst': str, 'titleType': str, 'primaryTitle': str, 'originalTitle': str, 'isAdult': str, 'startYear': str, 'endYear': str, 'runtimeMinutes': str, 'genres': str})
+    ratings = pd.read_csv('data/imdb/title.ratings.tsv', sep='\t', header=0, dtype={'tconst': str, 'averageRating': float, 'numVotes': int})
+
+    imdb = basics.merge(ratings, on="tconst", how="inner")
+    
+    # Remove lines where titleType is not "movie"
+    imdb = imdb[imdb['titleType'] == 'movie']
+    
+    # Remove lines where numVotes is less than 10000
+    imdb = imdb[imdb['numVotes'] >= 10000]
+    
+    # Sort the dataframe by rating
+    imdb = imdb.sort_values(by=["averageRating"], ascending=False)
+    
+    # write the data to a tsv file
+    imdb.to_csv('data/imdb/imdb.tsv', sep='\t', index=False)
+    
+
+# Reads the config.ini file and runs the main function.
+def read_config():
+    # Open the config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    main(config['IMDB'].getboolean('force_download'), config['IMDB'].getboolean('force_merge'))
+
+def main(force_download, force_merge):
     if (not os.path.exists("data/imdb")):
         os.makedirs("data/imdb")
         
-    if (not os.path.exists("data/imdb/title.ratings.tsv") or not os.path.exists("data/imdb/title.basics.tsv")):
+    # Download and unzip the imdb data if force_download is True in the config.
+    if (not os.path.exists("data/imdb/title.ratings.tsv") or not os.path.exists("data/imdb/title.basics.tsv") or force_download):
         print("Downloading and unzipping IMDB data...")
         download_and_unzip_imdb()
+    
+    if (not os.path.exists("data/imdb/imdb.tsv") or force_merge):
+        print("Matching tconst...")
+        match_tconst()
 
-main()
+read_config()
